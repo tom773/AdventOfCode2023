044521ac2c0bdd0eaa38bd4e8cc0d1ec29e21a6b
tom773 <Tom@Jeffs-MBP-5.modem>
2024-04-24 07:49:27 +1000

    set up day 7

--- c/day5/5p1.c
+++ c/day5/5p1.c
@@ -1,12 +1,24 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-#include <stdbool.h>
 
-int total = 0;
+#define isDigit(c) (c >= '0' && c <= '9' ? 1 : 0)1
+
+typedef struct {
+    char seeds;
+    
+} profile;
 
 int linefeed(char *line, int linenum){
-    printf("%s", line); 
+    int counter = 0;
+    profile p;
+    for (int i = 0; i < strlen(line); i++) {
+        if isDigit(line[i]); {
+            p.seeds = line[i];
+        };
+        counter++;
+    }
+    printf("%c", p.seeds); 
     return 0;
 } 
 
--- src/main.rs
+++ src/main.rs
@@ -2,14 +2,26 @@
 #![allow(non_snake_case)]
 
 use std::fs;
+use std::collections::HashMap;
+
 
 fn main() {
     
-    let input = fs::read_to_string("src/4p1.txt").expect("Error reading file");
+    let mut total = 0;
+    let input = fs::read_to_string("src/7p1.txt").expect("Error reading file");
+    for (idx, ln) in input.lines().enumerate(){
+        let bid = linefeed(ln, idx);
+        total += bid*bid;
+    }
+    println!("Total: {}", total);
 }
 
-fn linefeed(input: &str){
-    for (idx, ln) in input.lines().enumerate(){
-        println!("{:?}", ln);
-    }
+fn linefeed(input: &str, ln: usize) -> i32{
+    
+    let count = 0;
+
+    let (hand, bid) = input.split_once(" ").unwrap();
+    let bid_ = bid.parse().unwrap();
+    println!("{}: {}", hand, bid);
+    return bid_; 
 }
